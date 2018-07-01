60d49dc5eab3a34187c7586907a62a08252a4120
George Kapoya <george@immedia.co.za>
2018-07-01 15:27:13 +0200

    added sqlite db

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace DatingApp.API
 {
@@ -25,6 +29,8 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddMvc();
+             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
